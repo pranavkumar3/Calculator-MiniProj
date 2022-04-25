70a7a2a92bb646a2dc5d2a5729aa4f686b34be77
pranavkumar-373 <pranav.kumar@iiitb.org>
2022-04-25 21:34:19 +0530

    minor changes

--- src/test/java/Calculator/CalculatorTest.java
+++ src/test/java/Calculator/CalculatorTest.java
@@ -20,9 +20,9 @@ public class CalculatorTest {
 
     @Test
     public void power(){
-        double base = 5;
+        double base = 4;
         double power = 3;
-        double expected_result = 125;
+        double expected_result = 64;
         calculator.power(base,power);
         double result = calculator.get_ans();
         Assert.assertEquals(expected_result, result, 0.00001);
@@ -40,8 +40,8 @@ public class CalculatorTest {
     
     @Test
     public void testfactorial(){
-        double num = 5;
-        double expected_result = 120;
+        double num = 4;
+        double expected_result = 24;
         calculator.factorial(num);
         double result = calculator.get_ans();
         Assert.assertEquals(expected_result, result, 0.00001);
